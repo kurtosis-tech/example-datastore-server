385a86a99c320bf6f52897d5be22b30f953b45a2
Leandro Poroli <leandro.poroli@kurtosistech.com>
2021-11-01 11:53:08 -0300

    Added datastore_rpc_api_consts

--- api/typescript/src/index.ts
+++ api/typescript/src/index.ts
@@ -1,4 +1,5 @@
 export { EXAMPLE_DATASTORE_SERVER_VERSION } from "./example_datastore_server_version/example_datastore_server_version";
+export { LISTEN_PROTOCOL, LISTEN_PORT } from "./datastore_rpc_api_consts/datastore_rpc_api_consts";
 
 //Datastore RPC API Bindings
 export { DatastoreServiceClient } from "./datastore_rpc_api_bindings/datastore_service_grpc_pb";
